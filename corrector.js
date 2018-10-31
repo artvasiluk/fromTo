91641c3fced11ab137e50eb7d6296635810065d2
Artem Vasiluk <artvasiluk@gmail.com>
2018-10-31 14:24:16 +0200

    updated version

--- corrector.js
+++ corrector.js
@@ -1,66 +1,96 @@
 const fs = require('fs');
+const delComments = require('./delComments.js');
 
-
-
-function read(fielnameFrom, fielnameTo, cb) {
+function correctorFromTo(fielnameFrom, fielnameTo, cb) {
 	fs.readFile(fielnameFrom, function(err, data) {  
-		if (err) throw err;  
-		
+		if (err) throw err;
+
 		const contents = data.toString();
 		let str = contents;
-		console.log(str);
-		let newStr = '';
-		let count = 0;
-		
-		function tab(count) {
-			let tab = '';
-			for (let i = 0; i < count; i++) {
-				tab += '\t';
-			}
-			return tab;				
-		}
-		
-		function whiteSpace(symb) {
-			if (symb === ' ' || symb === '\t') {
-				return '';
-			}
-		}
-		
-		for (let i = 0; i < str.length; i++) {
-			newStr += str[i];
-
-			
-			if (str[i] === '{'&& str[i+1] !== '\r') {
-				newStr += '\r' + '\n' + tab(count + 1);
-				count++;
-				//console.log('Inc', i, count);
-			} else if (str[i] === ';' && str[i+1] !== '\r') {
-				if (str[i+1] === ' ' || str[i+1] === '\t') {
-					newStr += '';
-				} else if (str[i+1] !== '}') {
-					newStr += '\r' + '\n' + tab(count);
-				} else {
-					newStr += '\r' + '\n' + tab(count - 1);
-				}
-			} else if (str[i] === '}'&& str[i+1] !== '\r') {
-				if (str[i+1] !== '}') {
-					count--;
-					newStr += '\r' + '\n' + tab(count);
-					//console.log('Dec', i, count);
-				} else {
-					count--;
-					newStr += '\r' + '\n' + tab(count - 1);
-				}
-			}
-		}
-		console.log(newStr);
+		let newStr = delComments.delComment(str);
+		newStr = corrector(newStr);
+		cb(newStr, fielnameTo);
 	});
 }
 
-read('1.js', 'to.js', write);
-
 function write(str, fielnameTo) {
-	fs.writeFile(str, fielnameTo, function(err) {
+	fs.writeFile(fielnameTo, str, function(err) {
 		if (err) throw err; 
 	});
 }
+
+function corrector(str) {
+	let newStr = '';
+	let count = 0;
+	let state = 'code';
+	
+	function tab(count) {
+		let tab = '';
+		for (let i = 0; i < count; i++) {
+			tab += '\t';
+		}
+		return tab;				
+	}
+
+	for (let i = 0; i < str.length; i++) {
+		switch(state) {
+			case 'code':
+				if (str[i] === ';') {
+					newStr += str[i];					
+					state = 'symbol';
+				}else if (str[i] === '\n') {
+					state = 'symbol';
+				} else if (str[i] === '{') {
+					newStr += str[i];
+					state = 'symbol';
+					count++;
+				} else if (str[i] === '}') {
+					count--;
+					newStr += '\n' + tab(count) + str[i];
+				} else if (str[i] === '"') {
+					newStr += str[i];
+					state = 'quotes';
+				} else if (str[i] === "'") {
+					newStr += str[i];
+					state = 'quoteationMark';
+				}  else {
+					newStr += str[i];
+				}
+				break;
+			case 'symbol':
+				if (str[i] === '\r') {
+					newStr += str[i];
+					state = 'code';
+				} else if (str[i] === ' ' || str[i] === '\t') {
+					newStr += '';
+				} else if (str[i] === '}') {
+					count--;
+					newStr += '\n' + tab(count) + str[i];
+					state = 'code';
+				} else {
+					newStr += '\n' + tab(count) + str[i];
+					state = 'code';
+				}
+				break;
+			case 'quoteationMark':	
+				if (str[i] === "'" || str[i] === '\n') {
+					newStr += str[i];
+					state = 'code';
+				} else {
+					newStr += str[i];
+				}
+				break;
+			case 'quotes':
+				if (str[i] === '"' || str[i] === '\n') {
+					newStr += str[i];
+					state = 'code';	
+				} else {
+					newStr += str[i];
+				}
+				break;
+		}
+	}
+	return newStr;
+}
+
+correctorFromTo('from.js', 'to.js', write);
